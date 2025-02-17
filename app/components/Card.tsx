9266520162da750a27d543d383de93bf4cb4273a
Pola Samy <polasamy81311518@gmail.com>
2025-02-17 20:23:08 +0200

    2

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,14 +12,14 @@ const Card = () => {
       }}></Image>
       <h1>Jessica Randall</h1>
       <h2>London, United Kingdom</h2>
-      <h3>"Front-end developer and avid reader."</h3>
+      <h3>{"Front-end developer and avid reader."}</h3>
       {/*you cant use the Link from next as its only for internal links */}
-      <a href="https://github.com" target="blank">GitHub</a>
-      <a href="https://www.frontendmentor.io">Frontend Mentor</a>
-      <a href="https://www.linkedin.com">LinkedIn</a>
-      <a href="https://www.facebook.com">Facebook</a>
-      <a href="https://twitter.com">Twitter</a>
-      <a href="https://www.instagram.com">Instagram</a>
+      <a href="https://github.com" target="_blank">GitHub</a>
+      <a href="https://www.frontendmentor.io" target="_blank">Frontend Mentor</a>
+      <a href="https://www.linkedin.com" target="_blank">LinkedIn</a>
+      <a href="https://www.facebook.com" target="_blank">Facebook</a>
+      <a href="https://twitter.com" target="_blank">Twitter</a>
+      <a href="https://www.instagram.com" target="_blank">Instagram</a>
     </div>
   )
 }
