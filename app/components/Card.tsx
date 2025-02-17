1d2573ae955b12f18c6aa44cfd15810d80fcbdc4
Pola Samy <polasamy81311518@gmail.com>
2025-02-17 20:12:09 +0200

    done

---
+++ app/components/Card.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import Jessica from "@/app/images/avatar-jessica.jpeg"
+const Card = () => {
+  return (
+    <div className="container">
+      <Image src={Jessica} alt="Photo of Jessica"
+      style={{
+        width:"80px",
+        borderRadius:"50%",
+        marginTop:"10%",
+        marginBottom:"5%",
+      }}></Image>
+      <h1>Jessica Randall</h1>
+      <h2>London, United Kingdom</h2>
+      <h3>"Front-end developer and avid reader."</h3>
+      {/*you can use the Link from next as its only for internal links */}
+      <a href="https://github.com" target="blank">GitHub</a>
+      <a href="https://www.frontendmentor.io">Frontend Mentor</a>
+      <a href="https://www.linkedin.com">LinkedIn</a>
+      <a href="https://www.facebook.com">Facebook</a>
+      <a href="https://twitter.com">Twitter</a>
+      <a href="https://www.instagram.com">Instagram</a>
+    </div>
+  )
+}
+
+export default Card
